264af8dff6bdba2c8bf2b4a448deb1ee006b0e9d
= <=>
2012-03-20 22:29:25 +0800

    push ishow.me

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IshowMe::Application.config.secret_token = '461cfe04f70e39209c94825ae5e14fa8b20e9328470e389f7d89aaa65e20d3e13f05d248a8ff6c65d4c37572f54d1dae90d9b97e36b3cebd513411f912d7a0d7'
